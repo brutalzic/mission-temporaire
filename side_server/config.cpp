72196901e85c2d9174731970d7ed5c95d9ff3350
brutalzic <btrteam@outlook.com>
2016-09-22 01:35:35 +0200

    Create config.cpp

---
+++ side_server/config.cpp
@@ -0,0 +1,25 @@
+/*
+	File: config.cpp
+	Author: John Doe, Kira, Brutalzic
+*/
+class CfgPatches {
+    class side_server {
+        units[] = {};
+        weapons[] = {};
+        requiredAddons[] = {};
+        fileName = "side_server.pbo";
+        author[]= {"Brutalzic,Kira,John Doe"};
+    };
+};
+
+class CfgFunctions
+{
+    class DB_connect {
+        tag = "DBC";
+        class DataBase
+        {
+            file = "\side_server\Functions\DataBase";
+            class asyncCall {};
+		};
+	};
+};
